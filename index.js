41d8667c19dc9820b986b2dceb248206619fd4e2
Eyob Samuel <leeyoba@gmail.com>
2023-10-19 01:13:18 +0300

    install 3

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 // index.js
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoURL =
+  "mongodb+srv://root:root@cluster0.qytq2iu.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoURL);
+const app = express();
+const PORT = 4000;
 
-const app = express()
-const PORT = 4000
+const ProductSchema = new mongoose.Schema({
+  id: Number,
+  title: String,
+  price: Number,
+});
+
+const ProductModel = mongoose.model("Products", ProductSchema);
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+app.get("/", (req, res) => {
+  ProductModel.find({}).then((result) => {
+    res.send(result);
+  });
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
 
 // Export the Express API
-module.exports = app
+module.exports = app;
