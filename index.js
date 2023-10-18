cb8501bc3e78714e43cfdd73bbfcb39d61817a7c
Eyob Samuel <leeyoba@gmail.com>
2023-10-19 01:44:06 +0300

    install 3

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
+// Import necessary modules
 const express = require("express");
 const mongoose = require("mongoose");
 
+// Create an Express application
 const app = express();
-const mongoURL =
-  "mongodb+srv://root:root@cluster0.qytq2iu.mongodb.net/?retryWrites=true&w=majority";
+const port = 3000;
 
-mongoose.connect(mongoURL).then(() => {
+// Connect to MongoDB using Mongoose
+mongoose.connect(
+  "mongodb+srv://root:root@cluster0.qytq2iu.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hey, this is my API running 🥳");
+// Define a sample Mongoose model
+const Todo = mongoose.model("Todo", { text: String });
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Define a route to create a new todo
+app.post("/todos", async (req, res) => {
+  try {
+    const todo = new Todo({ text: req.body.text });
+    const savedTodo = await todo.save();
+    res.json(savedTodo);
+  } catch (error) {
+    res.status(500).json({ error: "Error creating todo" });
+  }
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is my about route.....");
+// Define a route to get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching todos" });
+  }
 });
 
-module.exports = app;
+// Define a route to get a single todo by ID
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching todo" });
+  }
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
