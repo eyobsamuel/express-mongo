8419b02bce8ebdcab704aac7fb2f42cbc48e02cf
Eyob Samuel <leeyoba@gmail.com>
2023-10-19 01:14:59 +0300

    install 3

--- index.js
+++ index.js
@@ -4,25 +4,16 @@ const mongoose = require("mongoose");
 const mongoURL =
   "mongodb+srv://root:root@cluster0.qytq2iu.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoURL);
+
 const app = express();
 const PORT = 4000;
-
-const ProductSchema = new mongoose.Schema({
-  id: Number,
-  title: String,
-  price: Number,
-});
-
-const ProductModel = mongoose.model("Products", ProductSchema);
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
 });
 
 app.get("/", (req, res) => {
-  ProductModel.find({}).then((result) => {
-    res.send(result);
-  });
+  res.send("Hey this is my API running 🥳");
 });
 
 app.get("/about", (req, res) => {
