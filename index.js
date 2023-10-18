bdc80ef58a041e4a42d01b86bca747f57c316e35
Eyob Samuel <leeyoba@gmail.com>
2023-10-19 01:11:12 +0300

    install 2

--- index.js
+++ index.js
@@ -1,64 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+// index.js
+const express = require('express')
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+const PORT = 4000
 
-const mongoURL =
-  "mongodb+srv://root:root@cluster0.qytq2iu.mongodb.net/?retryWrites=true&w=majority";
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
 
-mongoose
-  .connect(mongoURL)
-  .then(() => {
-    console.log("Mongo is Connected");
-  })
-  .catch(() => {
-    console.log("Mongo is not Connected");
-  });
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
 
-const ProductSchema = new mongoose.Schema({
-  id: Number,
-  title: String,
-  price: Number,
-});
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
-const ProductModel = mongoose.model("Products", ProductSchema);
-
-app.get("/", (req, res) => {
-  ProductModel.find({}).then((result) => {
-    res.send(result);
-  });
-});
-
-app.get("/search/:title", (req, res) => {
-  ProductModel.find({ title: req.params.title }).then((result) => {
-    res.send(result);
-  });
-});
-
-app.post("/create", (req, res) => {
-  const newProduct = req.body;
-  const prod1 = new ProductModel(newProduct);
-  prod1.save().then(() => res.status(201).send("Saved"));
-});
-
-app.patch("/update/:id", (req, res) => {
-  const keyId = +req.params.id;
-  const newProduct = req.body;
-  ProductModel.findOneAndUpdate({ id: keyId }, newProduct, { new: true }).then(
-    (result) => res.send(result)
-  );
-});
-
-app.delete("/remove/:id", (req, res) => {
-  const keyId = +req.params.id;
-  ProductModel.findOneAndDelete({ id: keyId }).then((result) =>
-    res.send(result)
-  );
-});
-
-app.listen(8080, function () {
-  console.log("Server is running on 8080");
-});
+// Export the Express API
+module.exports = app
